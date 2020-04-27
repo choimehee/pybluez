1e96ded37d9271f9bdb4bb1bd0e1b3fa9aa11854
choimehee <risio379@naver.com>
2020-04-28 00:06:00 +0900

    insert2

--- examples/simple/inquiry.py
+++ examples/simple/inquiry.py
@@ -10,13 +10,25 @@ $Id: inquiry.py 401 2006-05-05 19:07:48Z albert $
 
 import bluetooth
 import pymongo
+import collections
+import json
+from bson import json_util
 
-client=pymongo.MongoClient("mongodb://yechoi:0000@192.168.1.32:27017/")
+
+odbcArray=[]
+
+client=pymongo.MongoClient('mongodb://yechoi:0000@220.149.13.179:27017/admin')
+mydb=client["bluetooth"]
+scans=mydb["scans"]
+
+print('MongoDB Connected.')
 
 print("Performing inquiry...")
 
 nearby_devices = bluetooth.discover_devices(duration=8, lookup_names=True,
                                             flush_cache=True, lookup_class=False)
+
+
 
 print("Found {} devices".format(len(nearby_devices)))
 
@@ -25,3 +37,16 @@ for addr, name in nearby_devices:
         print("   {} - {}".format(addr, name))
     except UnicodeEncodeError:
         print("   {} - {}".format(addr, name.encode("utf-8", "replace")))
+
+#for addr,name in nearby_devices:
+#    doc=collections.OrderedDict()
+#    doc['address']=addr
+#    doc['deviceName']=name
+#    odbcArray.append(doc)
+
+#print(odbcArray)
+
+for addr in nearby_devices[0]:
+    scans.insert_one({"address":addr})
+
+mongoConnect.close()

8155da946e1454c0357b8e7feedf45d5cb662a28
choimehee <risio379@naver.com>
2020-04-28 00:16:29 +0900

    insert2

--- examples/simple/inquiry.py
+++ examples/simple/inquiry.py
@@ -11,27 +11,16 @@ $Id: inquiry.py 401 2006-05-05 19:07:48Z albert $
 import bluetooth
 import pymongo
 import collections
-<<<<<<< HEAD
-import json
 from bson import json_util
 
-
-odbcArray=[]
 
 client=pymongo.MongoClient('mongodb://yechoi:0000@220.149.13.179:27017/admin')
 mydb=client["bluetooth"]
 scans=mydb["scans"]
 
 print('MongoDB Connected.')
-=======
-from bson import json_util
-
-client=pymongo.MongoClient("mongodb://yechoi:0000@192.168.1.32:27017/")
-mydb=client["bluetooth"]
-scans=mydb["scans"]
 
 odbcArray=[]
->>>>>>> 648d9450d9bb6176b2c9be29fb3b344bc39ec774
 
 print("Performing inquiry...")
 
@@ -48,25 +37,12 @@ for addr, name in nearby_devices:
     except UnicodeEncodeError:
         print("   {} - {}".format(addr, name.encode("utf-8", "replace")))
 
-<<<<<<< HEAD
-#for addr,name in nearby_devices:
-#    doc=collections.OrderedDict()
-#    doc['address']=addr
-#    doc['deviceName']=name
-#    odbcArray.append(doc)
-
-#print(odbcArray)
 
 for addr in nearby_devices[0]:
     scans.insert_one({"address":addr})
 
-mongoConnect.close()
-=======
-for addr, name in nearby_devices:
-    doc=collections.OrderedDict()
-    doc['address']=addr
-    doc['deviceName']=name
-    odbcArray.append(doc)
-
-mongoImp=dbo.insert_many(odbcArray)
->>>>>>> 648d9450d9bb6176b2c9be29fb3b344bc39ec774
+#for addr, name in nearby_devices:
+#    doc=collections.OrderedDict()
+#    doc['address']=addr
+#    doc['deviceName']=name
+#    odbcArray.append(doc)
